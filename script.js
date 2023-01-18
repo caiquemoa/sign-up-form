e872e9a5533f194981c3bedfeac5584917672ee9
caiquemoa <caiquemoa@gmail.com>
2023-01-18 12:37:49 -0300

    form inputs style

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputs = [...document.getElementsByTagName('input')]
+const password = document.getElementsByName('password')[0]
+const password_confirmation = document.getElementsByName('confirm_password')[0]
+inputs.forEach((input) => {
+	input.addEventListener('invalid', () => input.classList.add('error'), false)
+})
+
+function confirmPassword() {
+	if (password.value !== password_confirmation.value) {
+		password_confirmation.setCustomValidity('password not matching')
+		password_confirmation.previousElementSibling.textContent =
+			'CONFIRM PASSWORD NOT MATCH'
+		password_confirmation.previousElementSibling.style.color = 'red'
+	} else {
+		password_confirmation.setCustomValidity('')
+		password_confirmation.classList.remove('pseudo-element')
+		password_confirmation.previousElementSibling.textContent =
+			'CONFIRM PASSWORD'
+		password_confirmation.previousElementSibling.style.color = 'black'
+	}
+}
+
+password.onchange = confirmPassword
+password_confirmation.onkeyup = confirmPassword
